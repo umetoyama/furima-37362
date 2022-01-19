4e0cda8e8d5fe6838354982263bf3b91522cc04a
umetoyama <dumeyama@gmail.com>
2022-01-19 16:59:16 +0900

    テスト系gemの追加/テストコード記述

---
+++ spec/factories/users.rb
@@ -0,0 +1,17 @@
+FactoryBot.define do
+  factory :user do
+
+    japanese_user =Gimei.name
+
+    nickname              {Faker::Name.name}
+    email                 {Faker::Internet.free_email}
+    password              {Faker::Internet.password(min_length: 6)}
+    password_confirmation {password}
+    last_name             {japanese_user.last.kanji}
+    first_name            {japanese_user.first.kanji}
+    last_name_read        {japanese_user.last.katakana}
+    first_name_read       {japanese_user.first.katakana}
+    birth_date            {Faker::Date.backward}
+    
+  end
+end
