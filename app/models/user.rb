e2b3a1eeb2d8112950609a5b47fedc28fe6232ca
umetoyama <dumeyama@gmail.com>
2022-01-19 14:50:05 +0900

    バリデーションの設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,14 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  with_options presence: true do
+    validates :nickname
+    validates :last_name
+    validates :first_name
+    validates :last_name_read
+    validates :first_name_read
+    validates :birth_date
+  end
+  
 end
