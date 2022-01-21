a0090f46c0be681350ace2c044e15a95d7fac273
umetoyama <dumeyama@gmail.com>
2022-01-21 13:58:32 +0900

    activehashを使用したモデルの作成と記述/アソシエーションの設定

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -9,8 +9,7 @@ class ItemsController < ApplicationController
   private
 
   def item_params
-    keys_to_allow == :product_name, :description, :category_id, :condition_id, :shipping_cost_id, :prefecture_id, :shipping_time_id, :price
-    params.require(:item).permit(keys_to_allow).merge(user_id: current_user.id)
+    params.require(:item).permit(:product_name, :description, :category_id, :condition_id, :shipping_cost_id, :prefecture_id, :shipping_time_id, :price).merge(user_id: current_user.id)
   end
 
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -2,8 +2,11 @@ class Item < ApplicationRecord
 
   has_many :items
   has_one_attached :image
-
-  
+  belongs_to :category
+  belongs_to :condition
+  belomgs_to :prefecture
+  belomgs_to :shipping_cost
+  belomgs_to :shipping_time
 
   with_options presence: true do
     validates :product_name
@@ -11,7 +14,7 @@ class Item < ApplicationRecord
     validates :price
   end
 
-  with_options numericality: { other_than: 1 } do
+  with_options numericality: { other_than: 1, message: "can't be blank" } do
     validates :category_id
     validates :condition_id
     validates :shipping_cost_id
