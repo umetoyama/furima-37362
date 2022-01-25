df5efce0552b59fd4490fca593691646ccbac5f1
umetoyama <dumeyama@gmail.com>
2022-01-25 12:25:09 +0900

    修正#3

--- app/models/item.rb
+++ app/models/item.rb
@@ -15,7 +15,7 @@ class Item < ApplicationRecord
     validates :image
   end
 
-  validates :price, presence: true, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 }, only_integer: true
+  validates :price, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 }
   # , format: { with: /\A[0-9]+\z/, message: "must be single-byte numbers only"}
 
   with_options numericality: { other_than: 1, message: "can't be blank" } do
