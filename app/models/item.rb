dcf193daab83de5ab475a5385476e56b14531a0e
umetoyama <dumeyama@gmail.com>
2022-01-25 11:20:31 +0900

    商品出品機能 修正#2

--- app/models/item.rb
+++ app/models/item.rb
@@ -15,7 +15,7 @@ class Item < ApplicationRecord
     validates :image
   end
 
-  validates :price, presence: true, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 }
+  validates :price, presence: true, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 }, only_integer: true
   # , format: { with: /\A[0-9]+\z/, message: "must be single-byte numbers only"}
 
   with_options numericality: { other_than: 1, message: "can't be blank" } do
